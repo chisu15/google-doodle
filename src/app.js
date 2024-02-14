abbd308861859347bf53d507f63dbfaac950cd50
chisu15 <trunghieudo1509@gmail.com>
2024-02-14 21:08:27 +0700

    Khoi tao du an

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const {default: helmet} = require('helmet');
+const compression = require('compression');
+const app = express();
+// init middlewares
+app.use( morgan("dev") )
+app.use( helmet() )
+app.use( compression() );
+// init db
+require('./dbs/init.mongodb')
+const {countConnect, checkOverload} = require('./helpers/check.connect')
+checkOverload();
+// init routes
+
+// handling error
+
+module.exports = app;
